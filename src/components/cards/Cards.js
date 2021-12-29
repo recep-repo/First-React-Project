85f989452cfeea14c46c0ea0c05c4105e1c1d702
recep-repo <recepedurmus@gmail.com>
2021-12-29 22:00:02 +0300

    commit

--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -1,46 +1,17 @@
-// import "./Cards.css"
-// import {data} from "../../helpers/data"
-
-// const Cards = () => {
-//     return (
-//         <div className="card">
-//             {
-//                 data.map((card, id) => {
-//                     return(
-//                     <div className="cards" key={id}>
-//                         <div className="title">
-//                             <h2>{card.title}</h2>
-//                         </div>
-//                         <img src={card.image} alt={card.title}/>
-//                         <div className="desc">
-//                             <p>{card.desc}</p>
-//                         </div>
-//                     </div>
-//                     );
-//                 })
-//             }
-//         </div>
-//     );
-// };
-
-// export default Cards;
-
 import "./Cards.css";
 import { data } from "../../helpers/data";
-console.log(data);
+
 const Cards = () => {
   return (
-    <div className="card">
+    <div className="card-container">
       {data.map((card, index) => {
         return (
           <div className="cards" key={index}>
             <div className="title">
-              <h2>{card.title}</h2>
+              <span className="card-title">{card.title}</span>
             </div>
             <img src={card.image} alt={card.title} />
-            <div className="desc">
-              <p>{card.desc}</p>
-            </div>
+            <h4 className='card-over'>{card.desc}</h4>
           </div>
         );
       })}
